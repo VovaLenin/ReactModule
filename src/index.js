a9bb7111ffe76f93d5038190705be6ce01931416
VovaLenin <ustinov2708@gmail.com>
2024-02-13 02:20:20 +0300

    moving files to another folder and preparing for gh pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals.js";
 import App from "./App.js";
 import "bootstrap/dist/css/bootstrap.css";
 
